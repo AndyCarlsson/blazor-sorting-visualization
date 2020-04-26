364d7d9c4806aa6835db6b12d42ddb37fad0bdad
AndyCarlsson <andreas95carlsson@gmail.com>
2020-04-26 15:07:44 +0200

    Moved BubbleSort to new class

---
+++ SortingVisualization/SortingVisualization/Shared/BubbleSortClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SortingVisualization.Shared
+{
+    public class BubbleSortClass : SortingComponent
+    {
+        public async Task BubbleSort(int[] intArr, SortingComponentBase sortingComponentBase)
+        {
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                for (int j = 0; j < intArr.Length - i - 1; j++)
+                {
+                    if (intArr[j] > intArr[j + 1])
+                    {
+                        Swap(intArr, j, j + 1);
+                        sortingComponentBase.UpdateUI();
+                        await Task.Delay(2);
+                    }
+                }
+            }
+        }
+        public void Swap(int[] arr, int a, int b)
+        {
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+    }
+}
--- SortingVisualization/SortingVisualization/Shared/SortingComponent.razor.cs
+++ SortingVisualization/SortingVisualization/Shared/SortingComponent.razor.cs
@@ -1,18 +1,32 @@
 ﻿using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SortingVisualization.Shared
 {
     public class SortingComponentBase : ComponentBase
     {
-        public static int[] numArr = new int[80];
+        public int[] numArr = new int[80];
+        
 
         protected override void OnInitialized()
         {
             FillArray();
+        }
+
+        public void UpdateUI()
+        {
+            this.StateHasChanged();
+        }
+
+        public async void CallBubbleSort()
+        {
+            BubbleSortClass bubbleSortClass = new BubbleSortClass();
+            await bubbleSortClass.BubbleSort(numArr, this);
         }
 
         public void FillArray()
@@ -27,29 +41,5 @@ namespace SortingVisualization.Shared
             }
             this.StateHasChanged();
         }
-
-        public async Task BubbleSort(int[] intArr)
-        {       
-            for (int i = 0; i < intArr.Length; i++)
-            {
-                for (int j = 0; j < intArr.Length - i - 1; j++)
-                {
-                    if (intArr[j] > intArr[j + 1])
-                    {
-                        Swap(intArr, j, j + 1);
-                        this.StateHasChanged();
-                        await Task.Delay(2);
-                    }
-                }
-            }        
-        }
-
-        public void Swap(int[] arr, int a, int b)
-        {
-            int temp = arr[a];
-            arr[a] = arr[b];
-            arr[b] = temp;
-        }
-
     }
 }
