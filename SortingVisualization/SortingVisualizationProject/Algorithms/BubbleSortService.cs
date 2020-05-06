1b3ebdd076a20aaba918ef21d4a2aad002f6c2ee
AndyCarlsson <andreas95carlsson@gmail.com>
2020-05-06 16:23:23 +0200

    Pasuing available for bubblesort

--- SortingVisualization/SortingVisualizationProject/Algorithms/BubbleSortService.cs
+++ SortingVisualization/SortingVisualizationProject/Algorithms/BubbleSortService.cs
@@ -3,18 +3,22 @@ using SortingVisualizationProject.Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SortingVisualizationProject.Algorithms
 {
     public class BubbleSortService
     {
-        public async Task BubbleSort(int[] intArr, MainPage mainPage)
+        public async Task BubbleSort(int[] intArr, MainPage mainPage, CancellationToken ct)
         {
             for (int i = 0; i < intArr.Length; i++)
             {
+                
                 for (int j = 0; j < intArr.Length - i - 1; j++)
                 {
+                    if (ct.IsCancellationRequested)
+                        break;
                     if (intArr[j] > intArr[j + 1])
                     {
                         Swap(intArr, j, j + 1);
