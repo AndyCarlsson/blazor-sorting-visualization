943b87ce6734255d1cf502af40e4a6ad534ef656
AndyCarlsson <andreas95carlsson@gmail.com>
2020-05-07 09:00:58 +0200

    Can pause merge sort

--- SortingVisualization/SortingVisualizationProject/Algorithms/MergeSortService.cs
+++ SortingVisualization/SortingVisualizationProject/Algorithms/MergeSortService.cs
@@ -2,13 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SortingVisualizationProject.Algorithms
 {
     public class MergeSortService
     {
-        public async Task Merge(int[] array, int lowIndex, int middleIndex, int highIndex, MainPage mainPage)
+        public async Task Merge(int[] array, int lowIndex, int middleIndex, int highIndex, MainPage mainPage, CancellationToken ct)
         {
             var left = lowIndex;
             var right = middleIndex + 1;
@@ -42,19 +43,21 @@ namespace SortingVisualizationProject.Algorithms
             for (var i = 0; i < tempArray.Length; i++)
             {
                 array[lowIndex + i] = tempArray[i];
+                if (ct.IsCancellationRequested)
+                    break;
                 mainPage.UpdateUI();
                 await Task.Delay(5);
             }
         }
-        public async Task MergeSortAsync(int[] array, int lowIndex, int highIndex, MainPage mainPage)
+        public async Task MergeSortAsync(int[] array, int lowIndex, int highIndex, MainPage mainPage, CancellationToken ct)
         {
             if (lowIndex < highIndex)
             {
                 var middleIndex = (lowIndex + highIndex) / 2;
 
-                await MergeSortAsync(array, lowIndex, middleIndex, mainPage);
-                await MergeSortAsync(array, middleIndex + 1, highIndex, mainPage);
-                await Merge(array, lowIndex, middleIndex, highIndex, mainPage);
+                await MergeSortAsync(array, lowIndex, middleIndex, mainPage, ct);
+                await MergeSortAsync(array, middleIndex + 1, highIndex, mainPage, ct);
+                await Merge(array, lowIndex, middleIndex, highIndex, mainPage, ct);
             }
         }
     }
